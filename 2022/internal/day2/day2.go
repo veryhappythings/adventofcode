8d9df27f49472567ee6c79ed246fa0d3412ceca0
Mac Chapman <mac@veryhappythings.co.uk>
2022-12-02 10:46:12 +0000

    Day 2 Part 2

--- 2022/internal/day2/day2.go
+++ 2022/internal/day2/day2.go
@@ -15,8 +15,18 @@ const (
 	Scissors
 )
 
-var VillianLookup map[string]Shape = map[string]Shape{"A": Rock, "B": Paper, "C": Scissors}
-var HeroLookup map[string]Shape = map[string]Shape{"X": Rock, "Y": Paper, "Z": Scissors}
+var villianLookup map[string]Shape = map[string]Shape{"A": Rock, "B": Paper, "C": Scissors}
+var heroLookup map[string]Shape = map[string]Shape{"X": Rock, "Y": Paper, "Z": Scissors}
+
+// Part2HeroLookup can be used to look up first XYZ for lose/draw/win, then what the villian picked to choose our pick
+var part2HeroLookup map[string]map[string]Shape = map[string]map[string]Shape{
+	// Lose
+	"X": map[string]Shape{"A": Scissors, "B": Rock, "C": Paper},
+	// Draw
+	"Y": map[string]Shape{"A": Rock, "B": Paper, "C": Scissors},
+	// Win
+	"Z": map[string]Shape{"A": Paper, "B": Scissors, "C": Rock},
+}
 
 type Round struct {
 	Villain Shape
@@ -74,8 +84,33 @@ func Part1(filename string) {
 		text := scanner.Text()
 		var round Round
 		match := re.FindStringSubmatch(text)
-		round.Villain = VillianLookup[match[1]]
-		round.Hero = HeroLookup[match[2]]
+		round.Villain = villianLookup[match[1]]
+		round.Hero = heroLookup[match[2]]
+		tournament = append(tournament, round)
+	}
+
+	total := 0
+	for _, round := range tournament {
+		total += round.Score()
+	}
+	fmt.Println(total)
+}
+func Part2(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var tournament []Round
+	re := regexp.MustCompile(`(\w) (\w)`)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		var round Round
+		match := re.FindStringSubmatch(text)
+		round.Villain = villianLookup[match[1]]
+		round.Hero = part2HeroLookup[match[2]][match[1]]
 		tournament = append(tournament, round)
 	}
 
