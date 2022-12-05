15dbc2be79021b48bd06d08ec5bbbec3cc870199
Mac Chapman <mac@veryhappythings.co.uk>
2022-12-05 15:25:34 +0000

    Day 3 part 2

--- 2022/cmd/root.go
+++ 2022/cmd/root.go
@@ -16,7 +16,7 @@ var inputFilename string
 var days [][]func(string) = [][]func(string){
 	{day1.Part1, day1.Part2},
 	{day2.Part1, day2.Part2},
-	{day3.Part1, day2.Part2},
+	{day3.Part1, day3.Part2},
 }
 
 var rootCmd = &cobra.Command{
--- 2022/internal/day3/day3.go
+++ 2022/internal/day3/day3.go
@@ -1,43 +1,68 @@
 package day3
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 const pos string = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func Part1(filename string) {
-    file, err := os.Open(filename)
-    if err != nil {
-        panic(err)
-    }
-    defer file.Close()
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
-    total := 0
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        text := scanner.Text()
-        first := text[:len(text)/2]
-        second := text[len(text)/2:]
-        fmt.Printf("%s -> %s, %s\n", text, first, second)
+	total := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		first := text[:len(text)/2]
+		second := text[len(text)/2:]
+		fmt.Printf("%s -> %s, %s\n", text, first, second)
 
-        if len(first) != len(second) {
-            panic("Backpack sizes not even")
-        }
+		if len(first) != len(second) {
+			panic("Backpack sizes not even")
+		}
 
-        for _, r := range first {
-            if strings.ContainsRune(second, r) {
-                total += strings.IndexRune(pos, r)
-                break
-            }
-        }
-    }
-    fmt.Println(total)
+		for _, r := range first {
+			if strings.ContainsRune(second, r) {
+				total += strings.IndexRune(pos, r)
+				break
+			}
+		}
+	}
+	fmt.Println(total)
 }
 
 func Part2(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
+	total := 0
+	var currentGroup []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		currentGroup = append(currentGroup, text)
+
+		if len(currentGroup) != 3 {
+			continue
+		}
+
+		for _, r := range currentGroup[0] {
+			if strings.ContainsRune(currentGroup[1], r) && strings.ContainsRune(currentGroup[2], r) {
+				total += strings.IndexRune(pos, r)
+				break
+			}
+		}
+		currentGroup = []string{}
+	}
+	fmt.Println(total)
 }
