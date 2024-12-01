da8effc38555c486534517e37e65a3b463ff687c
Mac Chapman <mac@veryhappythings.co.uk>
2024-12-01 14:20:09 +0000

    feat: 2023 day 4

--- 2023/internal/day4/day4.go
+++ 2023/internal/day4/day4.go
@@ -1,6 +1,31 @@
 package day4
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
 func Part1(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	whitespaceRe := regexp.MustCompile(`\s`)
+	for scanner.Scan() {
+		text := scanner.Text()
+		card := strings.SplitN(text, ":", 2)[1]
+		winnersStr, numbersStr := strings.SplitN(card, "|", 2)[0], strings.SplitN(card, "|", 2)[1]
+		winners := whitespaceRe.Split(winnersStr, -1)
+		numbers := whitespaceRe.Split(numbersStr, -1)
+		fmt.Println(winners)
+		fmt.Println(numbers)
+	}
 }
 
 func Part2(filename string) {
