d11dac892521c782d04179415cacb916f2a3cfef
Mac Chapman <mac@veryhappythings.co.uk>
2024-12-25 18:39:55 +0000

    refactor: factor out grid function

--- 2024/src/main/kotlin/day4/day4.kt
+++ 2024/src/main/kotlin/day4/day4.kt
@@ -2,26 +2,8 @@ package uk.co.veryhappythings.day4
 
 import java.io.File
 
-data class Point(val x: Int, val y: Int)
-
-/**
- * Read a grid from a file
- * The grid is accessible using `grid[y][x]`
- * @param filename The file to read
- * @param grid The grid
- */
-fun readGrid(filename: String) : MutableList<MutableList<Char>> {
-    val stream = File(filename).inputStream()
-    val grid = mutableListOf<MutableList<Char>>()
-    stream.bufferedReader().forEachLine {
-        val row = mutableListOf<Char>()
-        for (char in it) {
-            row.add(char)
-        }
-        grid.add(row)
-    }
-    return grid
-}
+import uk.co.veryhappythings.common.Point
+import uk.co.veryhappythings.common.readGrid
 
 fun possiblePaths(grid: MutableList<MutableList<Char>>, pathRemainder: String): MutableList<MutableList<Point>> {
     if (pathRemainder.isEmpty()) {
